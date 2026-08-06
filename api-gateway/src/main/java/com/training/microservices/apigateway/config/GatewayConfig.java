a815442b7762549f5049ba7946c9beeab2a76424
obmeraz <zarembo.valentine@gmail.com>
2026-08-06 18:28:20 +0200

    Module 5: Service discovery - Spring Cloud API Gateway was added

---
+++ api-gateway/src/main/java/com/training/microservices/apigateway/config/GatewayConfig.java
@@ -0,0 +1,30 @@
+package com.training.microservices.apigateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Value("${song.service.name}")
+    private String songServiceName;
+
+    @Value("${resource.service.name}")
+    private String resourceServiceName;
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(
+                        "resources", r -> r.path("/resources/**")
+                                .uri("lb://" + resourceServiceName)
+                ).route(
+                        "songs", r -> r.path("/songs/**")
+                                .uri("lb://" + songServiceName)
+                ).build();
+
+    }
+}
